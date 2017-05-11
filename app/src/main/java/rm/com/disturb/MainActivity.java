7fc44fb3bbf9a7ed53b41f1e4ea3f5a8d61d2db7
alex <rupta3@gmail.com>
2017-05-11 16:59:26 +0300

    Fixed permissions check

--- app/src/main/java/rm/com/disturb/MainActivity.java
+++ app/src/main/java/rm/com/disturb/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -14,18 +15,14 @@ import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import com.canelmas.let.AskPermission;
-import com.canelmas.let.DeniedPermission;
-import com.canelmas.let.Let;
-import com.canelmas.let.RuntimePermissionListener;
-import com.canelmas.let.RuntimePermissionRequest;
-import java.util.List;
 import javax.inject.Inject;
 
-public final class MainActivity extends AppCompatActivity implements RuntimePermissionListener {
+public final class MainActivity extends AppCompatActivity {
+  private static final int REQ_PHONE_LISTENER_PERMISSIONS = 1;
 
   @BindString(R.string.message_test_notification) String messageTestNotification;
   @BindString(R.string.description_test_notification) String descriptionTestNotification;
+  @BindString(R.string.permissions_rationale) String permissionsRationale;
   @BindColor(R.color.color_accent) int colorAccent;
 
   @BindView(R.id.button_test_call) ImageView testCall;
@@ -40,37 +37,43 @@ public final class MainActivity extends AppCompatActivity implements RuntimePerm
     ButterKnife.bind(this);
     ((DisturbApplication) getApplicationContext()).injector().inject(this);
 
-    authorizeIfNeeded();
+    if (isPhoneListenerPermissionGranted()) {
+      indicateNotificationsAvailable();
+    } else {
+      requestPhoneListenerPermissions();
+    }
   }
 
   @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
       @NonNull int[] grantResults) {
-    Let.handle(this, requestCode, permissions, grantResults);
+    if (requestCode != REQ_PHONE_LISTENER_PERMISSIONS) {
+      return;
+    }
+
+    if (isPhoneListenerPermissionGranted()) {
+      indicateNotificationsAvailable();
+    } else {
+      Toast.makeText(this, permissionsRationale, Toast.LENGTH_LONG).show();
+    }
   }
 
-  @Override public void onShowPermissionRationale(List<String> permissionList,
-      RuntimePermissionRequest permissionRequest) {
-    Toast.makeText(this, "This app would not work without these permissions", Toast.LENGTH_LONG)
-        .show();
-  }
-
-  @Override public void onPermissionDenied(List<DeniedPermission> deniedPermissionList) {
-  }
-
-  @AskPermission({
-      Manifest.permission.READ_PHONE_STATE
-  }) private void authorizeIfNeeded() {
+  private void indicateNotificationsAvailable() {
     description.setText(descriptionTestNotification);
     testCall.setColorFilter(colorAccent);
   }
 
   @OnClick(R.id.button_test_call) void onSendTestNotification() {
     if (!isPhoneListenerPermissionGranted()) {
-      authorizeIfNeeded();
+      requestPhoneListenerPermissions();
       return;
     }
 
     notifier.notify(messageTestNotification);
+  }
+
+  private void requestPhoneListenerPermissions() {
+    ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.READ_PHONE_STATE },
+        REQ_PHONE_LISTENER_PERMISSIONS);
   }
 
   private boolean isPhoneListenerPermissionGranted() {
