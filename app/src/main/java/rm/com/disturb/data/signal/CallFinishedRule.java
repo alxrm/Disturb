6c5b01b5ed10624efb08fb8fcfbe0e48e5e6af2f
Alex <rupta3@gmail.com>
2017-08-30 23:05:23 +0300

    Added dependencies for Rule implementations

--- app/src/main/java/rm/com/disturb/data/signal/CallFinishedRule.java
+++ app/src/main/java/rm/com/disturb/data/signal/CallFinishedRule.java
@@ -1,12 +1,35 @@
 package rm.com.disturb.data.signal;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import rm.com.disturb.data.contact.ContactBook;
+import rm.com.disturb.data.storage.Storage;
+import rm.com.disturb.data.telegram.command.Command;
+import rm.com.disturb.inject.qualifier.Erase;
+import rm.com.disturb.inject.qualifier.Update;
 
 /**
  * Created by alex
  */
 
 public final class CallFinishedRule implements Rule<MessageSignal> {
+
+  private final @NonNull Context context;
+  private final @NonNull ContactBook contactBook;
+  private final @NonNull Command<String> update;
+  private final @NonNull Command<Boolean> erase;
+  private final @NonNull Storage<MessageSignal> signalStorage;
+
+  public CallFinishedRule(@NonNull Context context, @NonNull ContactBook contactBook,
+      @NonNull @Update Command<String> update, @NonNull @Erase Command<Boolean> erase,
+      @NonNull Storage<MessageSignal> signalStorage) {
+    this.context = context;
+    this.contactBook = contactBook;
+    this.update = update;
+    this.erase = erase;
+    this.signalStorage = signalStorage;
+  }
+
   @Override public boolean shouldFollow(@NonNull MessageSignal item) {
     return item.type().equals(Signals.CALL_FINISHED);
   }
--- app/src/main/java/rm/com/disturb/data/signal/CallMissedRule.java
+++ app/src/main/java/rm/com/disturb/data/signal/CallMissedRule.java
@@ -1,12 +1,31 @@
 package rm.com.disturb.data.signal;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import rm.com.disturb.data.contact.ContactBook;
+import rm.com.disturb.data.storage.Storage;
+import rm.com.disturb.data.telegram.command.Command;
+import rm.com.disturb.inject.qualifier.Update;
 
 /**
  * Created by alex
  */
 
 public final class CallMissedRule implements Rule<MessageSignal> {
+
+  private final @NonNull Context context;
+  private final @NonNull ContactBook contactBook;
+  private final @NonNull Command<String> update;
+  private final @NonNull Storage<MessageSignal> signalStorage;
+
+  public CallMissedRule(@NonNull Context context, @NonNull ContactBook contactBook,
+      @NonNull @Update Command<String> update, @NonNull Storage<MessageSignal> signalStorage) {
+    this.context = context;
+    this.contactBook = contactBook;
+    this.update = update;
+    this.signalStorage = signalStorage;
+  }
+
   @Override public boolean shouldFollow(@NonNull MessageSignal item) {
     return item.type().equals(Signals.CALL_MISSED);
   }
